08b8348074727cc4eb7aed9b1925348b6ba4b47e
romanakhatun <romanakhatun2004@gmail.com>
2021-03-02 11:52:33 +0600

    initial commit

---
+++ src/components/questionnaire.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './quiz.scss';
+
+const Questionnaire = ({ handleAnswer, showAnswer, nextQuestion, data: { question, correct_answer, answers, incorrect_answers } }) => {
+
+    // const shuffledAnswer = [...incorrect_answers, correct_answer].sort(() => Math.random - 0.5);
+
+    return (
+        <div className="questionnaireSection">
+            <h4 dangerouslySetInnerHTML={{ __html: question }}></h4>
+
+            <div>
+                {answers.map((answer, idx) => {
+                    const bgColor = showAnswer ? answer === correct_answer ? 'correct' : 'incorrect' : 'normal';
+                    return <button id="quizOptions" key={idx} className={`${bgColor}`} onClick={() => handleAnswer(answer)} dangerouslySetInnerHTML={{ __html: answer }} />
+
+                })}
+                {showAnswer && <button className="btn" onClick={nextQuestion}>Next Question</button>}
+            </div>
+        </div>
+    );
+};
+
+
+
+export default Questionnaire;
